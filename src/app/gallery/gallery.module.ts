8ac7e33cae8b8d71653223cc2233106296abfbee
tomaszpankowski <--get-all>
2022-05-16 13:59:39 +0200

    layout and frontend

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { GalleryRoutingModule } from './gallery-routing.module';
 import { GalleryComponent } from './gallery/gallery.component';
+import { GalleryS1Component } from './gallery-s1/gallery-s1.component';
+import { GalleryS2Component } from './gallery-s2/gallery-s2.component';
 
 
 @NgModule({
   declarations: [
-    GalleryComponent
+    GalleryComponent,
+    GalleryS1Component,
+    GalleryS2Component
   ],
   imports: [
     CommonModule,
--- src/app/package/package.module.ts
+++ src/app/package/package.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { PackageRoutingModule } from './package-routing.module';
 import { PackageComponent } from './package/package.component';
+import { PackageS1Component } from './package-s1/package-s1.component';
+import { PackageS2Component } from './package-s2/package-s2.component';
+import { PackageS3Component } from './package-s3/package-s3.component';
 
 
 @NgModule({
   declarations: [
-    PackageComponent
+    PackageComponent,
+    PackageS1Component,
+    PackageS2Component,
+    PackageS3Component
   ],
   imports: [
     CommonModule,
